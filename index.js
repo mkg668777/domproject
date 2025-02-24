591332ad2666c81268fc871f63f68d3d917a0854
mkg668777 <mkg668777@gmail.com>
2025-02-24 23:31:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let divContent = document.getElementById("div-content");
+
+
+divContent.style.backgroundColor = "yellow";
+divContent.style.margin ="20px";
+divContent.style.padding = "10px";
+divContent.style.fontSize = "18px";
+divContent.style.fontWeight = "bold";
+divContent.style.height = "200px";
+divContent.style.width = "300px";
+
+
